39c323f88bc29e5954ab969022dc80001e39d9e5
janpaul <janpaul@elidon.net>
2021-01-11 17:24:03 +0100

    removes web vitals

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,9 @@
 import 'normalize.css/normalize.css'
 import '../styles/index.css'
 
-export const reportWebVitals = metric => {
-  console.log(metric)
-}
+// export const reportWebVitals = metric => {
+//   console.log(metric)
+// }
 
 const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />
 
