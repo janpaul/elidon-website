12e5ad1dbd976d71dc11c75197e80bac0aedb1bf
janpaul <janpaul@elidon.net>
2021-04-01 13:50:51 +0200

    adds more text to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,6 +41,20 @@ const Home = () => {
           </span>
         </section>
         <Subtitle>Full-stack software engineer based in (and around) Amsterdam.</Subtitle>
+        <p className={styles.bodyText}>
+          My name is Jan Paul and live in <a href="https://www.iamsterdam.com/en">Amsterdam</a>. I practice the martial
+          art <a href="http://www.worldtaekwondo.org/index.html">taekwondo</a> rather fanatically, and I run 5km per day
+          although I will never really do that just for fun. I’m trained as an{' '}
+          <a href="https://en.wikipedia.org/wiki/General_Dynamics_F-16_Fighting_Falcon">F-16</a> fighter pilot but at
+          some point I made the switch to my other hobby - writing software. My main passion is on optimising code and
+          this is why most of my clients are in the web, gaming & media space - any company that needs highly performant
+          software. For the geeks among you, I prefer the programming languages{' '}
+          <a href="https://javascript.info">Javascript</a> (<a href="https://reactjs.org">React</a>),{' '}
+          <a href="https://en.wikipedia.org/wiki/C%2B%2B">C++</a> and <a href="https://rust-lang.org">Rust</a>. Apart
+          from my software activities, I’m also responsible for the hiring process at companies within &quot;my&quot;
+          comfort zone (media, mostly), setting up development teams and making sure said teams are able to do their
+          jobs, without too many distractions.
+        </p>
         <Social />
       </section>
     </Layout>
