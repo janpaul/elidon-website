30f08fcd2c784db7a9f837a69bf41a09c31b09ee
janpaul <janpaul@elidon.net>
2021-04-01 17:00:06 +0200

    report web vitals to google analytics (i hope)

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -3,25 +3,19 @@ import Image from 'next/image'
 import styles from './avatar.module.css'
 import config from '../config.json'
 
-const { avatarInterval } = config
+const { avatarInterval, avatarImages } = config
 
-const AvatarImages = [
-  'jp.jpg',
-  'jp2.jpg',
-  'jp3.jpg',
-  'jp4.jpg',
-  'roos+jp.jpg',
-  'roos+jp2.jpg',
-  'roos+jp3.jpg',
-].map(image => <Image src={`/images/${image}`} alt="my avatar" width={256} height={256} className={styles.image} />)
+const AvatarImageComponents = [...avatarImages].map(image => (
+  <Image src={`/images/${image}`} alt="my avatar" width={256} height={256} className={styles.image} />
+))
 interface Props {
   onAvatarChange?: Function
 }
 
 const Avatar = ({ onAvatarChange }: Props) => {
-  const [image, setImage] = useState(AvatarImages[0])
+  const [image, setImage] = useState(AvatarImageComponents[0])
   const updateWithRandomImage = () => {
-    setImage(AvatarImages[Math.floor(Math.random() * AvatarImages.length)])
+    setImage(AvatarImageComponents[Math.floor(Math.random() * AvatarImageComponents.length)])
     if (!!onAvatarChange) onAvatarChange()
   }
   useEffect(() => {
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,29 @@
 import 'normalize.css/normalize.css'
 import '../styles/index.css'
 
-// export const reportWebVitals = metric => {
-//   console.log(metric)
-// }
+export const reportWebVitals = metric => {
+  if (!!dataLayer) {
+    dataLayer.push({
+      event: 'Vital Reported',
+      'vital.name': metric.name,
+      'vital.value': metric.value,
+      'vital.id': metric.id,
+    })
+  }
+}
+
+let baseStyles = [
+  'color: #bf616a',
+  'background-color: #2e3440',
+  'padding: 1rem 4px',
+  'border-radius: 2px',
+  'font-size: 18px',
+].join(';')
+
+console.log(
+  `%cHi there! Since you appear to be interested in the code of this website, why don't you go ahead and check out https://github.com/janpaul/elidon-website ? That saves you some time ;-)`,
+  baseStyles
+)
 
 const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />
 
