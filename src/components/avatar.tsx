14f46a130660d1136f56861558a4c5e727e1897c
janpaul <janpaul@elidon.net>
2021-04-01 14:40:07 +0200

    new images

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -5,7 +5,7 @@ import config from '../config.json'
 
 const { avatarInterval } = config
 
-const AvatarImages = ['jp.jpg', 'jp2.jpg', 'roos+jp.jpg'].map(image => (
+const AvatarImages = ['jp.jpg', 'jp2.jpg', 'jp3.jpg', 'jp4.jpg', 'roos+jp.jpg', 'roos+jp2.jpg'].map(image => (
   <Image src={`/images/${image}`} alt="my avatar" width={256} height={256} className={styles.image} />
 ))
 
