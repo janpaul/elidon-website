a53a7eda036da42e0c6ca4c815e77baddf182e86
janpaul <janpaul@elidon.net>
2021-04-23 22:52:01 +0200

    404 page improved

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,13 @@
 import { Layout } from '../components'
-import styles from './index.module.css'
+import styles from './404.module.css'
+import React from 'react'
 
 const FourOhForPage = () => {
   return (
     <Layout>
-      <section className={styles.wrapper}>
-        <h1>404 - Page Not Found</h1>
+      <section className="content centered">
+        <h1 className="title">404 - Page Not Found</h1>
+        <img src={`/images/panda.png`} className={styles.panda} alt="a stupid panda" />
       </section>
     </Layout>
   )
