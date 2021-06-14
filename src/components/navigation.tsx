4ea9ff1ccca7f52f407b232be27908300b8e14e8
janpaul <janpaul@elidon.net>
2021-06-14 16:09:02 +0200

    css.gg icons

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,23 +1,33 @@
 import React from 'react'
 import Link from 'next/link'
 import styles from '../styles/navigation.module.css'
-
 import Config from '../config'
+import { DownloadIcon, HomeIcon, BulbIcon } from './icons'
+
 const {
   features: { clients, cv },
 } = Config
 const Navigation = () => (
   <nav className={styles.navigation}>
     <Link href="/">
-      <a>.home(🏡)</a>
+      <a>
+        .home(
+        <HomeIcon />)
+      </a>
     </Link>
     <Link href={'/about'}>
-      <a>.about(❓)</a>
+      <a>
+        .about(
+        <BulbIcon />)
+      </a>
     </Link>
     {clients && <Link href={'/clients'}>.clients()</Link>}
     {cv && (
       <Link href={'/cv'}>
-        <a>.cv(📄)</a>
+        <a>
+          .cv(
+          <DownloadIcon />)
+        </a>
       </Link>
     )}
   </nav>
--- src/pages/cv/index.tsx
+++ src/pages/cv/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-
 import { Layout, MetaHeader } from '../../components'
+import { DownloadIcon } from '../../components/icons'
 
 const CvPage = () => (
   <Layout>
     <MetaHeader url="https://elidon.net/cv" title="janpaul.cv()" />
     <section className="content centered">
       <a href={'/cv.pdf'} download="cv jan paul stegeman.pdf">
-        Download ☁️
+        Download <DownloadIcon width={32} height={32} />
       </a>
     </section>
   </Layout>
