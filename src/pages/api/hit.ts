d4e798530cb2a893b6867e1b88b23a6de59f31d3
janpaul <janpaul@elidon.net>
2021-06-22 21:18:50 +0200

    test page, trying to grab IP's

--- src/pages/api/hit.ts
+++
@@ -1,8 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node'
-
-const hit = (request: VercelRequest, response: VercelResponse) => {
-  console.log(request.query)
-  response.status(200).send(JSON.stringify({ response }))
-}
-
-export default hit
