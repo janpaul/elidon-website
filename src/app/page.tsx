f98db44df106980a85e89c38160ee6cc57debb70
Jan Paul <janpaul@hey.com>
2023-11-03 22:47:40 +0100

    adds icons to contact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
 import NextImage from "next/image";
+import {
+  DevicePhoneMobileIcon,
+  EnvelopeIcon,
+} from "@heroicons/react/24/outline";
 import { Title, Paragraph, Block, Link } from "@/components";
 import { contact } from "@/data/contact";
 import me from "@/media/jp.jpg";
@@ -19,8 +23,14 @@ const Home = () => (
     />
     <Block>
       <div className="flex flex-row gap-2 md:gap-4">
-        <Link href={`mailto:${contact.email}`}>{contact.email}</Link>
-        <Link href={`tel:${contact.phone}`}>{contact.phone}</Link>
+        <div className="flex flex-row gap-1 items-center">
+          <DevicePhoneMobileIcon className="w-5 h-5" />
+          <Link href={`mailto:${contact.email}`}>{contact.email}</Link>
+        </div>
+        <div className="flex flex-row gap-1 items-center">
+          <EnvelopeIcon className="w-5 h-5" />
+          <Link href={`tel:${contact.phone}`}>{contact.phone}</Link>
+        </div>
       </div>
     </Block>
   </div>
