1f551edbb3156a932bdf9cf74a0fe94779cd9ad9
Jan Paul <janpaul@hey.com>
2023-11-03 22:42:59 +0100

    initial commit

---
+++ src/components/index.tsx
@@ -0,0 +1,24 @@
+import type { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+export const Title = ({ children }: Props) => (
+  <h1 className="text-6xl my-4">{children}</h1>
+);
+export const Block = ({ children }: Props) => (
+  <div className="text-xl my-2">{children}</div>
+);
+export const Paragraph = ({ children }: Props) => (
+  <p className="text-xl my-2">{children}</p>
+);
+
+type LinkProps = Props & { href: string };
+export const Link = ({ children, href }: LinkProps) => (
+  <a
+    className="text-blue-500 underline underline-offset-2 hover:no-underline transition-all duration-300"
+    href={href}
+  >
+    {children}
+  </a>
+);
---
+++ src/data/contact.ts
@@ -0,0 +1,9 @@
+type Contact = {
+  email: string;
+  phone: string;
+};
+
+export const contact: Contact = {
+  email: `janpaul@hey.com`,
+  phone: `+31 6 2926 7339`,
+};
