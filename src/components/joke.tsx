20b97e4d8609e368b392c36c88ea5a30325ed731
Jan Paul <janpaul@hey.com>
2023-11-16 14:34:35 +0100

    joke font size

--- src/components/joke.tsx
+++ src/components/joke.tsx
@@ -9,11 +9,14 @@ export const Joke = () => {
     refreshInterval: revalidate * 1000,
   });
   const loadJoke = async () => mutate(`/api/joke`).then(() => {});
-  return joke ?
-      <div className="flex flex-col cursor-pointer" onClick={loadJoke}>
-        {joke.split(`\n`).map((line, index) => (
-          <div key={index}>{line}</div>
-        ))}
-      </div>
-    : null;
+  return joke ? (
+    <div
+      className="flex flex-col cursor-pointer text-xl lg:text-2xl"
+      onClick={loadJoke}
+    >
+      {joke.split(`\n`).map((line, index) => (
+        <div key={index}>{line}</div>
+      ))}
+    </div>
+  ) : null;
 };
