c8eab9888dce0b36deae7814a8694db7e7492b24
Jan Paul <janpaul@hey.com>
2023-12-07 14:03:02 +0100

    changes mail icon again

--- src/components/social.tsx
+++ src/components/social.tsx
@@ -1,11 +1,12 @@
-import { DevicePhoneMobileIcon } from "@heroicons/react/24/outline";
+import {
+  DevicePhoneMobileIcon,
+  EnvelopeOpenIcon,
+} from "@heroicons/react/24/outline";
 import {
   GithubIcon,
-  HeyIcon,
   InstagramIcon,
   BunqIcon,
   RedditIcon,
-  AppleIcon,
 } from "@/components/icons";
 import { Link } from "@/components";
 import { contact } from "@/data/contact";
@@ -14,7 +15,7 @@ export const Social = () => (
   <div className="flex flex-row gap-2 md:gap-4 flex-wrap">
     <div className="flex flex-row gap-1 items-center">
       <Link href={`mailto:${contact.email}`}>
-        <AppleIcon />
+        <EnvelopeOpenIcon className="h-8 w-8 md:h-10 md:w-10 lg:h-12 lg:w-12 text-white" />
       </Link>
     </div>
     <div className="flex flex-row gap-1 items-center">
