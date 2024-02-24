3bc5bae6b1b257a142de9520b3af001a49f85f79
Jan Paul <janpaul@hey.com>
2024-02-24 13:18:20 +0100

    better colors in social

--- src/components/social.tsx
+++ src/components/social.tsx
@@ -4,29 +4,36 @@ import { contact } from "@/data/contact";
 
 import wa from "@/icons/whatsapp-color.svg";
 import hey from "@/icons/hey-color.svg";
-import apple from "@/icons/apple-color.svg";
 import github from "@/icons/github-color.svg";
 import instagram from "@/icons/instagram-color.svg";
 import bunq from "@/icons/bunq-color.svg";
 import reddit from "@/icons/reddit-color.svg";
+import signal from "@/icons/signal-color.svg";
+
+const { email, phoneFlat } = contact;
 
 export const Social = () => (
-  <div className="flex flex-row gap-2 md:gap-4 flex-wrap bg-white p-2 rounded-md w-fit bg-opacity-10">
+  <div className="flex flex-row gap-2 md:gap-4 flex-wrap bg-white p-2 rounded-md w-fit bg-opacity-80 dark:bg-opacity-20">
     <div className="flex flex-row gap-1 items-center">
-      <Link href={`//wa.me/31629267339`} isExternal>
+      <Link href={`//wa.me/${phoneFlat}`} isExternal>
         <NextImage src={wa.src} width={32} height={32} alt="Whatsapp" />
       </Link>
     </div>
     <div className="flex flex-row gap-1 items-center">
-      <Link href={`mailto:${contact.email}`} isExternal>
+      <Link href={`mailto:${email}`} isExternal>
         <NextImage src={hey.src} width={32} height={32} alt="Hey email" />
       </Link>
     </div>
     <div className="flex flex-row gap-1 items-center">
-      <Link href={`tel:${contact.phone}`} isExternal>
-        <NextImage src={apple.src} width={32} height={32} alt="Apple" />
+      <Link href={`//signal.me/#p/+${phoneFlat}`} isExternal>
+        <NextImage src={signal.src} width={32} height={32} alt="Signal" />
       </Link>
     </div>
+    {/*<div className="flex flex-row gap-1 items-center">*/}
+    {/*  <Link href={`tel:${contact.phone}`} isExternal>*/}
+    {/*    <NextImage src={apple.src} width={32} height={32} alt="Apple" />*/}
+    {/*  </Link>*/}
+    {/*</div>*/}
     <div className="flex flex-row gap-1 items-center">
       <Link href={`//github.com/${contact.github}`} isExternal>
         <NextImage src={github.src} width={32} height={32} alt="Github" />
