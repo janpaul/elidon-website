eb76c6e54a061b9b043ad142dd3338509500ab25
Jan Paul <janpaul@hey.com>
2024-02-24 13:53:10 +0100

    moves ava to the top

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -14,18 +14,18 @@ const Home = async ({ params: { lang } }: Props) => {
   return (
     <div>
       <Title>{t(`home.title`)}</Title>
-      <Paragraph size="md">{t(`home.p1`)}</Paragraph>
-      <Paragraph size="md">{t(`home.p2`)}</Paragraph>
-      <Paragraph size="md">{t(`home.p3`)}</Paragraph>
-      <Paragraph size="md">{t(`home.p4`)}</Paragraph>
-      <Divider className="my-5" />
+      <Divider />
       <Avatar
         src={me.src}
         size="lg"
         isBordered
         className="w-64 h-64 text-large"
       />
-      <Divider />
+      <Paragraph size="md">{t(`home.p1`)}</Paragraph>
+      <Paragraph size="md">{t(`home.p2`)}</Paragraph>
+      <Paragraph size="md">{t(`home.p3`)}</Paragraph>
+      <Paragraph size="md">{t(`home.p4`)}</Paragraph>
+      <Divider className="my-5" />
       <Block className="text-sm italic my-4">
         <Joke />
       </Block>
