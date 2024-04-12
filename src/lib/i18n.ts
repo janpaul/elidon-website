32883ba710af4aa716f0ddb385c927a0b8ece857
Jan Paul <janpaul@hey.com>
2024-04-12 10:11:03 +0200

    fixes translation bug

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,16 +1,19 @@
-import { flattenObject } from "@/lib";
+import { flattenObject, type AnyObject } from "@/lib";
+import nl from "@/i18n/nl.json";
+import en from "@/i18n/en.json";
+import se from "@/i18n/se.json";
 
 export type Language = `nl` | `en` | `se`;
 export type TranslateFn = (key: string) => string;
 
-const dictionaries = {
-  en: () => import("../i18n/en.json").then((module) => module.default),
-  nl: () => import("../i18n/nl.json").then((module) => module.default),
-  se: () => import("../i18n/se.json").then((module) => module.default),
+const dictionaries: Record<Language, AnyObject> = {
+  nl,
+  en,
+  se,
 };
 
 export const translate = async (lang: Language): Promise<TranslateFn> => {
-  const dict = flattenObject(await dictionaries[lang]());
+  const dict = flattenObject(dictionaries[lang]);
   return (key: string): string => {
     // @ts-ignore
     const value = dict[key];
--- src/lib/object.ts
+++ src/lib/object.ts
@@ -1,17 +1,19 @@
-type AnyObject = { [key: string]: any };
+export type AnyObject = { [key: string]: any };
 
 export function flattenObject(obj: AnyObject, prefix: string = ``): AnyObject {
-  return Object.keys(obj).reduce((acc: AnyObject, key: string) => {
-    const pre = prefix.length ? prefix + `.` : ``;
-    if (
-      typeof obj[key] === `object` &&
-      obj[key] !== null &&
-      !Array.isArray(obj[key])
-    ) {
-      Object.assign(acc, flattenObject(obj[key], pre + key));
-    } else {
-      acc[pre + key] = obj[key];
-    }
-    return acc;
-  }, {});
+  return obj
+    ? Object.keys(obj).reduce((acc: AnyObject, key: string) => {
+        const pre = prefix.length ? prefix + `.` : ``;
+        if (
+          typeof obj[key] === `object` &&
+          obj[key] !== null &&
+          !Array.isArray(obj[key])
+        ) {
+          Object.assign(acc, flattenObject(obj[key], pre + key));
+        } else {
+          acc[pre + key] = obj[key];
+        }
+        return acc;
+      }, {})
+    : {};
 }
