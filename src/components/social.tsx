cd149949c11a48d84c8e79641a1077f3ba10f4c0
Jan Paul <janpaul@hey.com>
2024-04-12 10:14:29 +0200

    adds soundcloud link

--- src/components/social.tsx
+++ src/components/social.tsx
@@ -9,6 +9,7 @@ import instagram from "@/icons/instagram-color.svg";
 import bunq from "@/icons/bunq-color.svg";
 import reddit from "@/icons/reddit-color.svg";
 import signal from "@/icons/signal-color.svg";
+import soundcloud from "@/icons/soundcloud-color.svg";
 
 const { email, phoneFlat } = contact;
 
@@ -27,6 +28,16 @@ export const Social = () => (
     <div className="flex flex-row gap-1 items-center">
       <Link href={`//signal.me/#p/+${phoneFlat}`} isExternal>
         <NextImage src={signal.src} width={32} height={32} alt="Signal" />
+      </Link>
+    </div>
+    <div className="flex flex-row gap-1 items-center">
+      <Link href="https://soundcloud.com/janpaul-418014217" isExternal>
+        <NextImage
+          src={soundcloud.src}
+          width={32}
+          height={32}
+          alt="Soundcloud"
+        />
       </Link>
     </div>
     {/*<div className="flex flex-row gap-1 items-center">*/}
