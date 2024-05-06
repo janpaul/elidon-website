357f621a650e890c84db45861187df12c8fb457c
Jan Paul <janpaul@hey.com>
2024-05-06 14:03:30 +0200

    removes more unneeded stuff

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,5 +4,4 @@ export * from "./snippet";
 export * from "./spacer";
 export * from "./language-select";
 export * from "./text-generate-effect";
-export * from "./typewriter-effect";
 export * from "./wavy-background";
