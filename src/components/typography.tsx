421b3a1ffc21b190b817bcaf9eab431bf04ce3a8
Jan Paul <janpaul@hey.com>
2025-03-25 10:14:58 +0100

    fix. the. build.

--- src/components/typography.tsx
+++ src/components/typography.tsx
@@ -71,7 +71,12 @@ export const TypographyListItem = ({ children, className }: Props) => (
 );
 
 export const TypographyInlineCode = ({ children, className }: Props) => (
-  <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+  <code
+    className={cn(
+      "relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold",
+      className,
+    )}
+  >
     {children}
   </code>
 );
