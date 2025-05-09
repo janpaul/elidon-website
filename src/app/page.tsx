a84d2c74a9a9bfcc539ceeeea835e33cf659187e
Jan Paul <janpaul@hey.com>
2025-05-09 14:28:57 +0200

    no minHeight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import {
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <TypographyH1>Jan Paul</TypographyH1>
         <TypographyH4>
