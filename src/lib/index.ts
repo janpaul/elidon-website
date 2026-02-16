6b32851f50e5dc5b5603526cc1d348cb4affdb23
Jan Paul <janpaul@elidon.net>
2026-02-16 11:48:16 +0100

    fixes birthdate

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,6 +2,6 @@ import dayjs from "dayjs";
 
 export const getYear = (): number => new Date().getFullYear();
 export const getAge = (): number => {
-  const birthDate = dayjs("1974-06-01");
+  const birthDate = dayjs("1974-06-11");
   return dayjs().diff(birthDate, "year");
 };
