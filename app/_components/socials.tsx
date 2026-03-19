de431e45a5e2ab19deffaf40de61429e939ebae3
Jan Paul <janpaul@elidon.net>
2026-03-19 15:13:21 +0100

    'slide into my DMs' text

--- app/_components/socials.tsx
+++ app/_components/socials.tsx
@@ -2,26 +2,31 @@ import { FaGithub, FaInstagram, FaReddit, FaApple } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
 
 export const Socials = () => (
-  <div className="flex gap-2 mt-4">
-    <Button variant="ghost" size="icon" asChild>
-      <a href="mailto:janpaul@elidon.net" target="_blank">
-        <FaApple className="h-6 w-6" />
-      </a>
-    </Button>
-    <Button variant="ghost" size="icon" asChild>
-      <a href="https://github.com/janpaul" target="_blank">
-        <FaGithub className="h-6 w-6" />
-      </a>
-    </Button>
-    <Button variant="ghost" size="icon" asChild>
-      <a href="https://instagram.com/janpaul" target="_blank">
-        <FaInstagram className="h-6 w-6" />
-      </a>
-    </Button>
-    <Button variant="ghost" size="icon" asChild>
-      <a href="https://reddit.com/u/janpaul74" target="_blank">
-        <FaReddit className="h-6 w-6" />
-      </a>
-    </Button>
-  </div>
+  <>
+    <div className="flex gap-2 mt-4">
+      <Button variant="ghost" size="icon" asChild>
+        <a href="mailto:janpaul@elidon.net" target="_blank">
+          <FaApple className="h-6 w-6" />
+        </a>
+      </Button>
+      <Button variant="ghost" size="icon" asChild>
+        <a href="//github.com/janpaul" target="_blank">
+          <FaGithub className="h-6 w-6" />
+        </a>
+      </Button>
+      <Button variant="ghost" size="icon" asChild>
+        <a href="//instagram.com/janpaul" target="_blank">
+          <FaInstagram className="h-6 w-6" />
+        </a>
+      </Button>
+      <Button variant="ghost" size="icon" asChild>
+        <a href="//reddit.com/u/janpaul74" target="_blank">
+          <FaReddit className="h-6 w-6" />
+        </a>
+      </Button>
+    </div>
+    <p className="text-sm text-muted-foreground">
+      No calls. No forms. Just slide into my DMs.
+    </p>
+  </>
 );
