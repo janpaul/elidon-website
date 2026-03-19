60d21e113269acee9bf96266c47c38c65eff3fc9
Jan Paul <janpaul@elidon.net>
2026-03-19 10:24:40 +0100

    feat: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>hi</div>;
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 }
