54dfd24774dc8387dfa71d35c1859756e8421d83
Jan Paul <janpaul@elidon.net>
2026-05-12 15:07:02 +0200

    grab interval from search params

--- app/erotigif/page.tsx
+++ app/erotigif/page.tsx
@@ -1,10 +1,16 @@
 import { getErotigifs } from "@/app/erotigif/helpers";
 import { Slideshow } from "@/app/erotigif/_components";
 
-const ErotigifPage = async () => {
+const ErotigifPage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) => {
+  const params = await searchParams;
+  const interval = params?.interval ? Number(params.interval) : 10_000;
   const images = await getErotigifs();
 
-  return <Slideshow images={images} interval={10_000} />;
+  return <Slideshow images={images} interval={interval} />;
 };
 
 export default ErotigifPage;
