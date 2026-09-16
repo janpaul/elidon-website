7e3d8333946ceaccd0ab62d4d11efe65f0a849f8
Jan Paul <janpaul@elidon.net>
2026-09-16 10:45:50 +0200

    adds new jokes, new mixes

---
+++ app/dj/mixes.ts
@@ -0,0 +1,10 @@
+import type { MixType } from "@/app/dj/types";
+
+export const mixes: MixType[] = [
+  { file: "REC013 - 25-03-2026", name: "25-03-2026" },
+  { file: "REC014 - 10-04-2026", name: "10-04-2026" },
+  { file: "REC015 - 20-08-2026", name: "20-08-2026" },
+  { file: "REC016 - 05-09-2026", name: "05-09-2026" },
+  { file: "REC0017.WAV", name: "09-09-2026" },
+  { file: "REC0018.WAV", name: "11-09-2026" },
+];
--- app/dj/page.tsx
+++ app/dj/page.tsx
@@ -1,16 +1,9 @@
 import { Mix } from "@/app/dj/_components";
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import { Joke, Socials, MyAvatar } from "@/app/_components";
-import type { MixType } from "@/app/dj/types";
+import { mixes } from "@/app/dj/mixes";
 
 export const revalidate = 300;
-
-const mixes: MixType[] = [
-  { file: "REC013 - 25-03-2026", name: "25-03-2026" },
-  { file: "REC014 - 10-04-2026", name: "10-04-2026" },
-  { file: "REC015 - 20-08-2026", name: "20-08-2026" },
-  { file: "REC016 - 05-09-2026", name: "05-09-2026" },
-];
 
 const DJPage = () => (
   <div className="flex flex-col gap-2 md:gap-3 lg:gap-4">
